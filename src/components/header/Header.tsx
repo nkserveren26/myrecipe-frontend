aae41c124060c2ac2f01db09f08d38be66d3d5d5
nkserveren26 <norinoritoshi@outlook.jp>
2024-06-30 15:01:48 +0900

    ヘッダーの設定

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import { AppBar } from "@mui/material";
+import { Toolbar } from "@mui/material";
+import {Typography} from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
+import React from 'react';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000', // プライマリカラーを黒色に指定
+    },
+  },
+  typography: {
+    fontFamily: "Georgia",
+  },
+});
+
+type HeaderProps = {
+    title:string
+};
+
+export const Header:FC<HeaderProps> = (props) => {
+    const { title } = props; 
+    return (
+      <ThemeProvider theme={theme}>
+          <AppBar position="sticky">
+            <Toolbar disableGutters={true}>
+              <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                {title}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+      </ThemeProvider>
+    );
+};
