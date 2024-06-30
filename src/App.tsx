a8a36f6611b68d277579a9541e970dc4fdbc0629
nkserveren26 <norinoritoshi@outlook.jp>
2024-07-01 08:18:16 +0900

    ページの両端に余白を設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/header/Header';
+import { Box } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <Header title='My Recipe' />
+      <Box sx={{ marginLeft: '3%', marginRight: '3%' }}>
+        <Header title='My Recipe' />
+      </Box>
     </div>
   );
 }
