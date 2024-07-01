a2e46f79f664bbbb3156ef34666a6abe35567a65
nkserveren26 <norinoritoshi@outlook.jp>
2024-07-02 08:35:30 +0900

    トップページのヘッダー画像の大きさを調整

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,30 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/header/Header';
 import { Box, Typography } from '@mui/material';
-import HeaderImage from "./pexels-janetrangdoan-1099680.jpg";
+import HomeHeaderImage from "./HomeHeaderImage.jpg";
 
 function App() {
   return (
     <div className="App">
-      <Box sx={{ marginLeft: '3%', marginRight: '3%' }}>
+      <Box sx={{ marginLeft: '4%', marginRight: '4%' }}>
         <Header title='My Recipe' />
         <Box
           sx={{
             position: 'relative',
-            width: '1200px',
-            height: '800px',
-            margin: '2%',
+            width: '100%',
+            height: '500px',
             overflow: 'hidden',
+            marginTop: '2%'
           }}
         >
           <Box
             component="img"
-            src={HeaderImage}
+            src={HomeHeaderImage}
             alt="説明画像"
             sx={{
               width: '100%',
               height: '100%',
               objectFit: 'cover',
-              filter: 'grayscale(80%)',
               position: 'absolute',
               top: 0,
               left: 0,
