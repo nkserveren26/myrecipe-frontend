c5c5c3ac342235dd61b6f20dd6106ab95fdd4e90
nkserveren26 <norinoritoshi@outlook.jp>
2024-07-03 21:49:12 +0900

    Topページコンポーネント作成

---
+++ src/components/pages/Top.tsx
@@ -0,0 +1,57 @@
+import { Box, Typography } from '@mui/material';
+import HomeHeaderImage from "../../HomeHeaderImage.jpg";
+import React from 'react';
+
+export const Top: React.FC = () => {
+    return (
+        <>
+            <Box
+              sx={{
+                position: 'relative',
+                width: '100%',
+                height: '600px',
+                overflow: 'hidden',
+                marginTop: '2%',
+                marginBottom: '4%',
+                borderRadius: '8px',
+              }}
+            >
+              <Box
+                component="img"
+                src={HomeHeaderImage}
+                alt="説明画像"
+                sx={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  zIndex: 1,
+                }}
+              />
+              <Box
+                sx={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  color: 'white',
+                  zIndex: 2,
+                  textAlign: 'center',
+                }}
+              >
+                <Typography variant="h3" sx={{ fontWeight: 'bold', marginBottom: '10px' }}>
+                  My Recipe
+                </Typography>
+                <Typography variant="body1" sx={{ fontSize: '24px' }}>
+                  This app is my favorite recipe list.
+                </Typography>
+              </Box>
+            </Box>
+            <Box>
+              <Typography paddingBottom={3} fontWeight="bold" variant="h4">Recipe Category</Typography>
+            </Box>
+        </>
+    )
+}
