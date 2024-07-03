29181c4a045441394e1ed140738f005a7101184b
nkserveren26 <norinoritoshi@outlook.jp>
2024-07-03 21:50:13 +0900

    React Routerの定義を追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,59 +3,20 @@ import './App.css';
 import { Header } from './components/header/Header';
 import { Box, Typography } from '@mui/material';
 import HomeHeaderImage from "./HomeHeaderImage.jpg";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Top } from './components/pages/Top';
 
 function App() {
   return (
     <div className="App">
       <Box sx={{ marginLeft: '4%', marginRight: '4%' }}>
-        <Header title='My Recipe' />
-        <Box
-          sx={{
-            position: 'relative',
-            width: '100%',
-            height: '600px',
-            overflow: 'hidden',
-            marginTop: '2%',
-            marginBottom: '4%',
-            borderRadius: '8px',
-          }}
-        >
-          <Box
-            component="img"
-            src={HomeHeaderImage}
-            alt="説明画像"
-            sx={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              zIndex: 1,
-            }}
-          />
-          <Box
-            sx={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              color: 'white',
-              zIndex: 2,
-              textAlign: 'center',
-            }}
-          >
-            <Typography variant="h3" sx={{ fontWeight: 'bold', marginBottom: '10px' }}>
-              My Recipe
-            </Typography>
-            <Typography variant="body1" sx={{ fontSize: '24px' }}>
-              This app is my favorite recipe list.
-            </Typography>
-          </Box>
-        </Box>
-        <Box>
-          <Typography paddingBottom={3} fontWeight="bold" variant="h4">Recipe Category</Typography>
-        </Box>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Header title='My Recipe' />
+          <Routes>
+            <Route path="/" element={<Top />}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </Box>
     </div>
   );
