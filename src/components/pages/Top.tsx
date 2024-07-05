1fcdaf429943f2cc0664156f34a2c6dd1fe51248
nkserveren26 <norinoritoshi@outlook.jp>
2024-07-05 22:16:50 +0900

    Topページの不要な部分削除

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -64,51 +64,6 @@ export const Top: React.FC = () => {
                 </Button>
                 <Grid columns={{ xs: 6, sm: 8, md: 12 }} pt={4} container columnSpacing={6} alignItems="center" justifyContent="center">
                   <Grid item xs={6} sm="auto" md="auto" pb={6}>
-                        <Button
-                            onClick={handleButtonClick}
-                            sx={{
-                                position: 'relative',
-                                width: '400px',
-                                height: '150px',
-                                padding: 0,
-                                borderRadius: '8px', // 長方形の角を丸くする場合
-                                overflow: 'hidden',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                textTransform: 'none', // ボタンラベルのテキスト変換を無効にする
-                                '&:hover .backgroundImage': {
-                                    transform: 'scale(1.1)', // 背景画像を少し拡大
-                                    transition: 'transform 0.5s ease', // アニメーションの遷移を設定
-                                },
-                            }}
-                        >
-                            <Box
-                                component="img"
-                                src={HomeHeaderImage}
-                                className="backgroundImage"
-                                sx={{
-                                    position: 'absolute',
-                                    width: '100%',
-                                    height: '100%',
-                                    objectFit: 'cover',
-                                    zIndex: 1,
-                                    transition: 'transform 0.5s ease',
-                                }}
-                            />
-                            <Typography
-                                variant="h6"
-                                sx={{
-                                    position: 'relative',
-                                    zIndex: 2,
-                                    color: 'white', // 必要に応じてテキストカラーを変更
-                                    fontWeight: 'bold',
-                                    fontSize: '30px',
-                                }}
-                            >
-                                お肉
-                            </Typography>
-                        </Button>
                         <RecipeCategoryButton buttonLabelName="お肉" buttonBackgroundImage={HomeHeaderImage} destinationUrlPath="/" />
                   </Grid>
                 </Grid>
