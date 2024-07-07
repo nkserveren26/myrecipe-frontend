c8e4e54c62d0f371dc9fcaaccc00185f3eb74246
nkserveren26 <norinoritoshi@outlook.jp>
2024-07-07 16:49:29 +0900

    Topページのメイン画像部分をHomeHeaderに変更

--- src/components/page/Top.tsx
+++ src/components/page/Top.tsx
@@ -5,55 +5,18 @@ import HomeHeaderImage from "../../images/HomeHeaderImage.jpg";
 import React from 'react';
 import { RecipeCategoryButton } from '../button/RecipeCategoryButton';
 import { RecipeCategoryButtonPropsList } from '../variable/ArrayVariables';
+import { HeroHeader } from '../header/HeroHeader';
 
 export const Top: React.FC = () => {
 
     return (
         <>
-            <Box
-              sx={{
-                position: 'relative',
-                width: '100%',
-                height: '600px',
-                overflow: 'hidden',
-                marginTop: '2%',
-                marginBottom: '4%',
-                borderRadius: '8px',
-              }}
-            >
-              <Box
-                component="img"
-                src={HomeHeaderImage}
-                alt="説明画像"
-                sx={{
-                  width: '100%',
-                  height: '100%',
-                  objectFit: 'cover',
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  zIndex: 1,
-                }}
-              />
-              <Box
-                sx={{
-                  position: 'absolute',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
-                  color: 'white',
-                  zIndex: 2,
-                  textAlign: 'center',
-                }}
-              >
-                <Typography variant="h3" sx={{ fontWeight: 'bold', marginBottom: '10px' }}>
-                  My Recipe
-                </Typography>
-                <Typography variant="body1" sx={{ fontSize: '24px' }}>
-                  This app is my favorite recipe list.
-                </Typography>
-              </Box>
-            </Box>
+            <HeroHeader
+              backgroundImage={HomeHeaderImage}
+              title="My Recipe"
+              description="This app is my favorite recipe list."
+              descriptionFontSize={24}
+            />
             <Box>
               <Typography paddingBottom={3} fontWeight="bold" variant="h4">Recipe Category</Typography>
                 <Button variant="contained" startIcon={<AddIcon />}>
