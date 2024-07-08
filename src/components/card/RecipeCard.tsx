4160f6a1354f438b1b09e32a5013269043ae82d2
nkserveren26 <norinoritoshi@outlook.jp>
2024-07-09 08:47:35 +0900

    RecipeCardに編集ボタンと削除ボタンを追加

--- src/components/card/RecipeCard.tsx
+++ src/components/card/RecipeCard.tsx
@@ -1,4 +1,6 @@
-import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Box, Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import styled from "styled-components";
 import React from 'react';
 import { RecipeCardProps } from '../interface/interface';
@@ -8,9 +10,17 @@ export const RecipeCard: React.FC<RecipeCardProps> = ({
     title,
     registerdDate
 }) => {
+    const onEdit = () => {
+        console.log("Editting Recipe");
+    }
+
+    const onDelete = () => {
+        console.log("Deleting Recipe");
+    }
+
     return (
         <>
-            <StyledCard style={{ cursor: 'pointer' }} sx={{ width: 300, height: 280 }}>
+            <StyledCard style={{ cursor: 'pointer' }} sx={{ width: 300, height: 340 }}>
               <CardMedia
                 sx={{ height: 180 }}
                 image={recipeImage}
@@ -29,6 +39,19 @@ export const RecipeCard: React.FC<RecipeCardProps> = ({
                   {registerdDate}
                 </Typography>
               </CardContent>
+              <CardActions
+                sx={{
+                      justifyContent: 'flex-start',
+                      padding: 0,
+                }}
+              >
+                <IconButton onClick={onEdit} aria-label="edit" sx={{ color: 'blue' }}>
+                  <EditIcon />
+                </IconButton>
+                <IconButton onClick={onDelete} aria-label="delete" sx={{ color: 'red' }}>
+                  <DeleteIcon />
+                </IconButton>
+              </CardActions>
             </StyledCard>
         </>
     )
