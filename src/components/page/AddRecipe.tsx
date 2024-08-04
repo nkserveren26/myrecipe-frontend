e0b6d6d477c9a7111524f19dd1d39851fa979767
nkserveren26 <norinoritoshi@outlook.jp>
2024-08-04 14:20:38 +0900

    必須ラベルを料理名ラベルの横に表示

--- src/components/page/AddRecipe.tsx
+++ src/components/page/AddRecipe.tsx
@@ -14,7 +14,9 @@ export const AddRecipe: React.FC = () => {
             <Box component="form" onSubmit={handleSubmit} sx={{  margin: 'auto', pt: 5 }}>
                 <Typography variant="h4" sx={{ fontWeight: 'bold', mb: 2, textAlign: 'left' }}>レシピの追加</Typography>
 
-                <Typography variant="h6" sx={{ fontWeight: 'bold', mt: 2, textAlign: 'left' }}>料理名</Typography>
+                <Typography variant="h6" sx={{ fontWeight: 'bold', mt: 2, textAlign: 'left' }}>
+                    料理名 <Typography component="span" sx={{ color: 'red', fontSize: '18px' }}> (必須) </Typography>
+                </Typography>
                 <TextField
                     fullWidth
                     value={recipeName}
