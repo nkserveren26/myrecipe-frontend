c4c78ca919e290f1323da681b1e8a709921a16d3
nkserveren26 <norinoritoshi@outlook.jp>
2024-08-04 14:11:04 +0900

    料理名のテキストボックスを追加

--- src/components/page/AddRecipe.tsx
+++ src/components/page/AddRecipe.tsx
@@ -1,7 +1,9 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 export const AddRecipe: React.FC = () => {
+
+    const [recipeName, setRecipeName] = useState('');
 
     const handleSubmit = () => {
         // ここでフォームの送信処理を行います。
@@ -13,6 +15,15 @@ export const AddRecipe: React.FC = () => {
                 <Typography variant="h4" sx={{ fontWeight: 'bold', mb: 2, textAlign: 'left' }}>レシピの追加</Typography>
 
                 <Typography variant="h6" sx={{ fontWeight: 'bold', mt: 2, textAlign: 'left' }}>料理名</Typography>
+                <TextField
+                    fullWidth
+                    value={recipeName}
+                    onChange={(e) => setRecipeName(e.target.value)}
+                    variant="outlined"
+                    margin="normal"
+                    placeholder="例: 鮭のムニエル"
+                    sx={{ border: '1px solid', borderRadius: '8px' }}
+                />
             </Box>
         </>
     );
