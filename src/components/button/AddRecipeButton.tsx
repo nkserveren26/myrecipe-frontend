591e534d2f173196e19b1daaff858e87ac1b0c88
nkserveren26 <norinoritoshi@outlook.jp>
2024-08-28 22:30:58 +0900

    AddRecipeButton修正。クリックするとレシピ新規作成画面に遷移するように処理を追加。

--- src/components/button/AddRecipeButton.tsx
+++ src/components/button/AddRecipeButton.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AddRecipeButton: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/recipes/new");
+    };
     return (
         <>
-            <Button variant="contained" startIcon={<AddIcon />}>
+            <Button variant="contained" startIcon={<AddIcon />} onClick={handleClick}>
                 Add Recipe
             </Button>
         </>
