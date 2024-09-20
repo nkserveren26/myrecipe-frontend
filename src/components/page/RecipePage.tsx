3322b006da7ef6a7d4f8b73e7ae87556cbbd3683
nkserveren26 <norinoritoshi@outlook.jp>
2024-09-20 22:29:47 +0900

    RecipePage修正。レシピの詳細情報を取得するAPIを実行する処理を追加。

--- src/components/page/RecipePage.tsx
+++ src/components/page/RecipePage.tsx
@@ -14,6 +14,8 @@ export const RecipePage: React.FC = () => {
 
     const [recipeDetail, setRecipeDetail] = useState<RecipeDetail>();
 
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const fetchRecipeDetail = async () => {
             if (!process.env.REACT_APP_GET_RECIPE_DETAIL_URL) {
@@ -27,21 +29,20 @@ export const RecipePage: React.FC = () => {
                 setRecipeDetail(response.data);
             } catch (error) {
                 console.error(`Error getting recipe ${id} detail:`, error);
+            } finally {
+                setLoading(false);  // ローディング完了
             }
         };
 
         fetchRecipeDetail();
     }, []);
 
-    // 正規表現を使用してIDが整数であることを確認
-    const isInteger = (str: string) => /^[0-9]+$/.test(str);
 
-    if (!id || !isInteger(id)) {
-        return <div>Invalid recipe ID</div>;
-    };
+    // データ取得中は以下の画面を表示
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-    // IDを10進数として解釈して整数に変換
-    const recipeId = parseInt(id, 10);
 
     if (!recipeDetail) {
         return <div>Recipe not found</div>;
@@ -49,72 +50,79 @@ export const RecipePage: React.FC = () => {
 
     return (
         <>
-            <Box pt={3} pb={5}>
-                <Box display="flex" alignItems="center" justifyContent="space-between" pb={3}>
-                    <Box flex={1} display="flex" justifyContent="center">
-                        <Typography pl={15} fontWeight="bold" variant="h4">
-                            {recipeDetail.title}
+            {recipeDetail ? (
+                <div>
+                    <Box pt={3} pb={5}>
+                        <Box display="flex" alignItems="center" justifyContent="space-between" pb={3}>
+                            <Box flex={1} display="flex" justifyContent="center">
+                                <Typography pl={15} fontWeight="bold" variant="h4">
+                                    {recipeDetail.title}
+                                </Typography>
+                            </Box>
+                            <DropdownButton />
+                        </Box>
+                        <Box display="flex" justifyContent="center">
+                            <ReactPlayer
+                                url={recipeDetail.videoUrl}
+                                controls={true}
+                                width="100%"
+                                height="500px"
+                            />
+                        </Box>
+                    </Box>
+                    <Box>
+                        <Typography pb={1} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
+                            材料
+                        </Typography>
+                        <Typography variant="body1" sx={{ textAlign: 'left', fontSize: '16px' }}>2人前</Typography>
+                        <Box sx={{ marginTop: 2 }}>
+                            {recipeDetail.ingredients.map((ingredient, index) => (
+                                <Grid container key={index} sx={{ marginBottom: 2 }}>
+                                    <Grid item xs={6} style={{ textAlign: 'left' }}>
+                                        <Typography variant="body1" sx={{ fontSize: '20px' }}>{ingredient.name}</Typography>
+                                    </Grid>
+                                    <Grid item xs={6} style={{ textAlign: 'right' }}>
+                                        <Typography variant="body1" sx={{ fontSize: '20px' }}>{ingredient.amount}</Typography>
+                                    </Grid>
+                                </Grid>
+                            ))}
+                        </Box>
+                    </Box>
+                    <Box mt={7}>
+                        <Typography pb={1} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
+                            作り方
+                        </Typography>
+                        <Box mt={2} >
+                            {recipeDetail.steps.map((step, index) => (
+                                <Grid container key={index} sx={{ marginBottom: 3 }}>
+                                    <Grid item xs={step.stepNumber === 0 ? 0.6 : 0.4} style={{ textAlign: 'left' }}>
+                                        <Typography variant="body1" sx={{ fontSize: '19px', fontWeight: 'bold' }}>
+                                            {step.stepNumber === 0 ? '準備' : step.stepNumber}.
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={step.stepNumber === 0 ? 11.4 : 11.6} style={{ textAlign: 'left' }}>
+                                        <Typography variant="body1" sx={{ fontSize: '20px' }}>
+                                            {step.description}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            ))}
+                        </Box>
+                    </Box>
+                    <Box mt={6}>
+                        <Typography pb={2} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
+                            料理のコツ・ポイント
+                        </Typography>
+                        <Typography variant="body1" sx={{ fontSize: '20px', textAlign: 'left', whiteSpace: 'pre-line' }}>
+                            {recipeDetail.point}
                         </Typography>
                     </Box>
-                    <DropdownButton />
-                </Box>
-                <Box display="flex" justifyContent="center">
-                    <ReactPlayer 
-                        url={recipeDetail.videoUrl} 
-                        controls={true} 
-                        width="100%"
-                        height="500px"
-                    />
-                </Box>
-            </Box>
-            <Box>
-                <Typography pb={1} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
-                    材料
-                </Typography>
-                <Typography variant="body1" sx={{ textAlign: 'left', fontSize: '16px' }}>2人前</Typography>
-                <Box sx={{ marginTop: 2 }}>
-                    {recipeDetail.ingredients.map((ingredient, index) => (
-                        <Grid container key={index} sx={{ marginBottom: 2 }}>
-                            <Grid item xs={6} style={{ textAlign: 'left' }}>
-                                <Typography variant="body1" sx={{ fontSize: '20px' }}>{ingredient.name}</Typography>
-                            </Grid>
-                            <Grid item xs={6} style={{ textAlign: 'right' }}>
-                                <Typography variant="body1" sx={{ fontSize: '20px' }}>{ingredient.amount}</Typography>
-                            </Grid>
-                        </Grid>
-                    ))}
-                </Box>
-            </Box>
-            <Box mt={7}>
-                <Typography pb={1} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
-                    作り方
-                </Typography>
-                <Box mt={2} >
-                    {recipeDetail.steps.map((step, index) => (
-                        <Grid container key={index} sx={{ marginBottom: 3 }}>
-                            <Grid item xs={step.stepNumber === 0 ? 0.6 : 0.4} style={{ textAlign: 'left' }}>
-                                <Typography variant="body1" sx={{ fontSize: '19px', fontWeight: 'bold' }}>
-                                    {step.stepNumber === 0 ? '準備' : step.stepNumber}.
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={step.stepNumber === 0 ? 11.4 : 11.6} style={{ textAlign: 'left' }}>
-                                <Typography variant="body1" sx={{ fontSize: '20px' }}>
-                                    {step.description}
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    ))}
-                </Box>
-            </Box>
-            <Box mt={6}>
-                <Typography pb={2} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
-                    料理のコツ・ポイント
-                </Typography>
-                <Typography variant="body1" sx={{ fontSize: '20px', textAlign: 'left', whiteSpace: 'pre-line' }}>
-                    {recipeDetail.point}
-                </Typography>
-            </Box>
-            <ScrollToTopButton />
+                    <ScrollToTopButton />
+                </div>
+            ) : (
+                <div>No recipe data found.</div>
+            )}
+            
         </>
     );
 }
