8184700ad3bc2dc939a76ff049736e0265dbb0d4
nkserveren26 <norinoritoshi@outlook.jp>
2024-10-03 08:51:06 +0900

    ErrorMessageコンポーネント作成。

---
+++ src/components/common/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+
+type ErrorMessageProps = {
+    message: string;
+};
+
+export const ErrorMessage: React.FC<ErrorMessageProps> = () => {
+    return (
+        <></>
+    )
+}
