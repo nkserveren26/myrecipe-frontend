1514caab2aaa695747b0d3209fc07e2cc1e138e1
nkserveren26 <norinoritoshi@outlook.jp>
2024-10-08 08:17:13 +0900

    AddRecipeButton修正。画面遷移時にstateで現在のカテゴリを渡す。

--- src/components/button/AddRecipeButton.tsx
+++ src/components/button/AddRecipeButton.tsx
@@ -2,12 +2,13 @@ import { Button } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { AddRecipeButtonProps } from "../interface/interface";
 
-export const AddRecipeButton: React.FC = () => {
+export const AddRecipeButton: React.FC<AddRecipeButtonProps> = ({ currentCategory }) => {
     const navigate = useNavigate();
 
     const handleClick = () => {
-        navigate("/recipes/new");
+        navigate("/recipes/new", { state: { category: currentCategory } });
     };
     return (
         <>
