6c910c6ca9514a3e327b0b9f1f8f30ccdf5ccd09
nkserveren26 <norinoritoshi@outlook.jp>
2024-12-12 11:41:22 +0900

    RecipeDeleteDialog修正。キャンセル時のクローズ処理を追加。

--- src/components/dialog/RecipeDeleteDialog.tsx
+++ src/components/dialog/RecipeDeleteDialog.tsx
@@ -1,11 +1,19 @@
 import { Button, Dialog, DialogActions, DialogTitle } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { RecipeDeleteDialogProps } from "../interface/interface";
 
 export const RecipeDeleteDialog: React.FC<RecipeDeleteDialogProps> = ({openDialog, setOpenDialog, recipeId}) => {
 
-    const handleDialogClose = () => {
-        setOpenDialog(false);
+    const [completeDialogOpen, setCompleteDialogOpen] = useState(false);
+
+    const handleCancelDialog = () => {
+        setOpenDialog(false); // ダイアログを閉じる
+    };
+
+
+    const handleCloseDialog = () => {
+        setCompleteDialogOpen(false);
+        setOpenDialog(false); // ダイアログを閉じる
     };
 
     const handleConfirmDelete = async () => {
@@ -23,14 +31,24 @@ export const RecipeDeleteDialog: React.FC<RecipeDeleteDialogProps> = ({openDialo
 
     return (
         <>
-            <Dialog open={openDialog} onClose={handleDialogClose}>
+            <Dialog open={openDialog} onClose={handleCloseDialog}>
                 <DialogTitle>本当に削除しますか？</DialogTitle>
                 <DialogActions>
-                    <Button onClick={handleDialogClose} variant="outlined" color="primary">
+                    <Button onClick={handleCancelDialog} variant="outlined" color="primary">
                         キャンセル
                     </Button>
                     <Button onClick={handleConfirmDelete} variant="contained" color="secondary">
                         削除
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* 削除完了ダイアログ */}
+            <Dialog open={completeDialogOpen} onClose={handleCloseDialog}>
+                <DialogTitle>レシピの削除が完了しました！</DialogTitle>
+                <DialogActions sx={{ justifyContent: 'center' }}>
+                    <Button onClick={handleCloseDialog} color="primary" variant="contained">
+                        Close
                     </Button>
                 </DialogActions>
             </Dialog>
