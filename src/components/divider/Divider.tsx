d8eeb4624e8d234cab5bbc53067f6a8029752881
nkserveren26 <norinoritoshi@outlook.jp>
2024-12-21 09:14:54 +0900

    Dividerコンポーネント作成。

---
+++ src/components/divider/Divider.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export const Divider: React.FC = () => {
+    return (
+        <>
+            < Box sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)', marginY: 2 }} />
+        </>
+    );
+}
