3342b8e0c9d3584c322223bd0680e4ab2f14ed9c
nkserveren26 <norinoritoshi@outlook.jp>
2024-12-23 09:32:28 +0900

    DividerWithColor修正。

--- src/components/divider/DividerWithColor.tsx
+++ src/components/divider/DividerWithColor.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { DividerWithColorProps } from "../interface/interface";
 
 export const DividerWithColor: React.FC<DividerWithColorProps> = ({
@@ -8,6 +9,28 @@ export const DividerWithColor: React.FC<DividerWithColorProps> = ({
     textWidth = '20%',
 }) => {
     return (
-        <></>
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                width: '100%',
+                my: marginY,
+            }}
+        >
+            {/* 左側（色付きで表示） */}
+            <Box
+                sx={{
+                    width: textWidth,
+                    borderBottom: `${thickness} solid ${colorLeft}`,
+                }}
+            />
+            {/* 右側（透明に表示） */}
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    borderBottom: `${thickness} solid ${colorRight}`,
+                }}
+            />
+        </Box>
     );
 }
