37a3cb0c6ca7199ec3bb31174773363ce926701e
nkserveren26 <norinoritoshi@outlook.jp>
2024-12-23 09:29:53 +0900

    DividerWithColorコンポーネント作成。

---
+++ src/components/divider/DividerWithColor.tsx
@@ -0,0 +1,13 @@
+import { DividerWithColorProps } from "../interface/interface";
+
+export const DividerWithColor: React.FC<DividerWithColorProps> = ({
+    colorLeft = 'red',
+    colorRight = 'rgba(0, 0, 0, 0)',
+    thickness = '1px',
+    marginY = 2,
+    textWidth = '20%',
+}) => {
+    return (
+        <></>
+    );
+}
