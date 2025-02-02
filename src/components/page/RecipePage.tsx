84b623eee79e2f5b647c561dcae53ed88a9fdedc
nkserveren26 <norinoritoshi@outlook.jp>
2025-02-02 09:04:40 +0900

    RecipePage修正。return部分の二項演算子を削除。

--- src/components/page/RecipePage.tsx
+++ src/components/page/RecipePage.tsx
@@ -103,121 +103,120 @@ export const RecipePage: React.FC = () => {
         );
     }
 
+    if (!recipeDetail) {
+        return null; // 何も表示しない
+    }
 
     return (
         <>
-            {recipeDetail ? (
-                <div>
-                    <Box pt={3} pb={5}>
-                        <Box display="flex" alignItems="center" justifyContent="space-between" pb={3}>
-                            <Box flex={1} display="flex" justifyContent="center">
-                                <Typography pl={15} fontWeight="bold" variant="h4">
-                                    {recipeDetail.title}
-                                </Typography>
-                            </Box>
-                            <Box>
-                                <Button
-                                    aria-controls="simple-menu"
-                                    aria-haspopup="true"
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={handleClick}
-                                    endIcon={<ArrowDropDownIcon />}
-                                >
-                                    Action
-                                </Button>
-                                <Menu
-                                    id="simple-menu"
-                                    anchorEl={anchorEl} // メニューのアンカー要素を設定
-                                    keepMounted
-                                    open={Boolean(anchorEl)} // anchorElがnullでない場合、メニューを表示
-                                    onClose={handleClose}
-                                >
-                                    <MenuItem onClick={handleEdit}>Edit</MenuItem>
-                                    <MenuItem onClick={handleDelete}>Delete</MenuItem>
-                                </Menu>
-                                
-                            </Box>
-                        </Box>
-                        <Box display="flex" justifyContent="center">
-                            <ReactPlayer
-                                url={recipeDetail.videoUrl}
-                                controls={true}
-                                width="100%"
-                                height="500px"
-                            />
-                        </Box>
-                    </Box>
-                    <Box>
-                        <Typography pb={0.5} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
-                            {`材料（${recipeDetail.servings}人前）`}
-                        </Typography>
-                        <DividerWithColor colorLeft="rgba(255, 99, 71, 1)" colorRight="rgba(0, 0, 0, 0.1)" thickness="2px" marginY={0} textWidth="20%" />
-                        <Box sx={{ marginTop: 4 }}>
-                            {recipeDetail.ingredients.map((ingredient, index) => (
-                                <Box key={index}>
-                                    <Grid container sx={{ marginBottom: 2 }}>
-                                        <Grid item xs={6} style={{ textAlign: 'left' }}>
-                                            <Typography variant="body1" sx={{ fontSize: '18px' }}>{ingredient.name}</Typography>
-                                        </Grid>
-                                        <Grid item xs={6} style={{ textAlign: 'right' }}>
-                                            <Typography variant="body1" sx={{ fontSize: '18px' }}>{ingredient.amount}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                    {/* 項目の下に線を挿入 */ }
-                                    <Divider color="rgba(0, 0, 0, 0.1)" thickness="1px" marginY={2} />
-                                </Box>
-                            ))}
-                        </Box>
-                    </Box>
-                    <Box mt={7}>
-                        <Typography pb={0.5} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
-                            作り方
-                        </Typography>
-                        <DividerWithColor colorLeft="rgba(255, 99, 71, 1)" colorRight="rgba(0, 0, 0, 0.1)" thickness="2px" marginY={0} textWidth="20%" />
-                        <Box mt={4} >
-                            {recipeDetail.steps.map((step, index) => (
-                                <Box key={index}>
-                                    <Grid container sx={{ marginBottom: 2 }}>
-                                        <Grid item xs={step.stepNumber === 0 ? 0.6 : 0.4} style={{ textAlign: 'left' }}>
-                                            <Typography variant="body1" sx={{ fontSize: '18px', fontWeight: 'bold' }}>
-                                                {step.stepNumber === 0 ? '準備' : step.stepNumber}.
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item xs={step.stepNumber === 0 ? 11.4 : 11.6} style={{ textAlign: 'left' }}>
-                                            <Typography variant="body1" sx={{ fontSize: '18px' }}>
-                                                {step.description}
-                                            </Typography>
-                                        </Grid>
-                                    </Grid>
-                                    {/* 項目の下に線を挿入 */}
-                                    <Divider color="rgba(0, 0, 0, 0.1)" thickness="1px" marginY={2} />
-                                </Box>
-                            ))} 
-                        </Box>
-                    </Box>
-                    <Box mt={6}>
-                        <Typography pb={0.5} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
-                            コツ・ポイント
-                        </Typography>
-                        <DividerWithColor colorLeft="rgba(255, 99, 71, 1)" colorRight="rgba(0, 0, 0, 0.1)" thickness="2px" marginY={0} textWidth="20%" />
-                        <Typography variant="body1" sx={{ fontSize: '18px', textAlign: 'left', whiteSpace: 'pre-line' }} pt={4}>
-                            {recipeDetail.point}
-                        </Typography>
-                    </Box>
-                    <ScrollToTopButton />
-
-                    {/* レシピ編集用ダイアログ */}
-                    <RecipeEditDialog openDialog={openEditDialog} setOpenDialog={setOpenEditDialog} recipeDetail={recipeDetail} />
-
-                    {/* 削除確認ダイアログ */}
-                    <RecipeDeleteDialog openDialog={openDeleteDialog} setOpenDialog={setOpenDeleteDialog} recipeId={recipeDetail.id} />
-
-                </div>
-            ) : (
-                <div>No recipe data found.</div>
-            )}
             
+            <div>
+                <Box pt={3} pb={5}>
+                    <Box display="flex" alignItems="center" justifyContent="space-between" pb={3}>
+                        <Box flex={1} display="flex" justifyContent="center">
+                            <Typography pl={15} fontWeight="bold" variant="h4">
+                                {recipeDetail.title}
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <Button
+                                aria-controls="simple-menu"
+                                aria-haspopup="true"
+                                variant="contained"
+                                color="primary"
+                                onClick={handleClick}
+                                endIcon={<ArrowDropDownIcon />}
+                            >
+                                Action
+                            </Button>
+                            <Menu
+                                id="simple-menu"
+                                anchorEl={anchorEl} // メニューのアンカー要素を設定
+                                keepMounted
+                                open={Boolean(anchorEl)} // anchorElがnullでない場合、メニューを表示
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleEdit}>Edit</MenuItem>
+                                <MenuItem onClick={handleDelete}>Delete</MenuItem>
+                            </Menu>
+                            
+                        </Box>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <ReactPlayer
+                            url={recipeDetail.videoUrl}
+                            controls={true}
+                            width="100%"
+                            height="500px"
+                        />
+                    </Box>
+                </Box>
+                <Box>
+                    <Typography pb={0.5} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
+                        {`材料（${recipeDetail.servings}人前）`}
+                    </Typography>
+                    <DividerWithColor colorLeft="rgba(255, 99, 71, 1)" colorRight="rgba(0, 0, 0, 0.1)" thickness="2px" marginY={0} textWidth="20%" />
+                    <Box sx={{ marginTop: 4 }}>
+                        {recipeDetail.ingredients.map((ingredient, index) => (
+                            <Box key={index}>
+                                <Grid container sx={{ marginBottom: 2 }}>
+                                    <Grid item xs={6} style={{ textAlign: 'left' }}>
+                                        <Typography variant="body1" sx={{ fontSize: '18px' }}>{ingredient.name}</Typography>
+                                    </Grid>
+                                    <Grid item xs={6} style={{ textAlign: 'right' }}>
+                                        <Typography variant="body1" sx={{ fontSize: '18px' }}>{ingredient.amount}</Typography>
+                                    </Grid>
+                                </Grid>
+                                {/* 項目の下に線を挿入 */ }
+                                <Divider color="rgba(0, 0, 0, 0.1)" thickness="1px" marginY={2} />
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
+                <Box mt={7}>
+                    <Typography pb={0.5} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
+                        作り方
+                    </Typography>
+                    <DividerWithColor colorLeft="rgba(255, 99, 71, 1)" colorRight="rgba(0, 0, 0, 0.1)" thickness="2px" marginY={0} textWidth="20%" />
+                    <Box mt={4} >
+                        {recipeDetail.steps.map((step, index) => (
+                            <Box key={index}>
+                                <Grid container sx={{ marginBottom: 2 }}>
+                                    <Grid item xs={step.stepNumber === 0 ? 0.6 : 0.4} style={{ textAlign: 'left' }}>
+                                        <Typography variant="body1" sx={{ fontSize: '18px', fontWeight: 'bold' }}>
+                                            {step.stepNumber === 0 ? '準備' : step.stepNumber}.
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={step.stepNumber === 0 ? 11.4 : 11.6} style={{ textAlign: 'left' }}>
+                                        <Typography variant="body1" sx={{ fontSize: '18px' }}>
+                                            {step.description}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                {/* 項目の下に線を挿入 */}
+                                <Divider color="rgba(0, 0, 0, 0.1)" thickness="1px" marginY={2} />
+                            </Box>
+                        ))} 
+                    </Box>
+                </Box>
+                <Box mt={6}>
+                    <Typography pb={0.5} fontWeight="bold" variant="h5" sx={{ textAlign: 'left' }}>
+                        コツ・ポイント
+                    </Typography>
+                    <DividerWithColor colorLeft="rgba(255, 99, 71, 1)" colorRight="rgba(0, 0, 0, 0.1)" thickness="2px" marginY={0} textWidth="20%" />
+                    <Typography variant="body1" sx={{ fontSize: '18px', textAlign: 'left', whiteSpace: 'pre-line' }} pt={4}>
+                        {recipeDetail.point}
+                    </Typography>
+                </Box>
+                <ScrollToTopButton />
+
+                {/* レシピ編集用ダイアログ */}
+                <RecipeEditDialog openDialog={openEditDialog} setOpenDialog={setOpenEditDialog} recipeDetail={recipeDetail} />
+
+                {/* 削除確認ダイアログ */}
+                <RecipeDeleteDialog openDialog={openDeleteDialog} setOpenDialog={setOpenDeleteDialog} recipeId={recipeDetail.id} />
+
+            </div>
         </>
     );
 }
