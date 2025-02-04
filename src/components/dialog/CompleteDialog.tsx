477254c80e832b800331fa5ebff6617bc798b669
nkserveren26 <norinoritoshi@outlook.jp>
2025-02-04 22:16:43 +0900

    CompleteDialog修正。ダイアログ部分を実装。

--- src/components/dialog/CompleteDialog.tsx
+++ src/components/dialog/CompleteDialog.tsx
@@ -1,5 +1,20 @@
-export const CompleteDialog: React.FC = () => {
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material"
+import { CompleteDialogProps } from "../interface/interface"
+
+export const CompleteDialog: React.FC<CompleteDialogProps> = ({ open, title, message, onClose }) => {
     return (
-        <></>
+        <>
+            <Dialog open={open} onClose={onClose}>
+                <DialogTitle>{title}</DialogTitle>
+                <DialogContent>
+                    <p>{message}</p>
+                </DialogContent>
+                <DialogActions sx={{ justifyContent: 'center' }}>
+                    <Button onClick={onClose} color="primary" variant="contained">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     )
 }
