3df93907c7e6f05cee5e82b2310e41ba63c86728
nkserveren26 <norinoritoshi@outlook.jp>
2025-02-08 13:13:26 +0900

    RecipeListPage修正。コンポーネント部分を実装。#15

--- src/components/page/RecipeListPage.tsx
+++ src/components/page/RecipeListPage.tsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import { RecipeCardProps, RecipeListPageProps } from "../interface/interface"
 import { useNavigate } from "react-router-dom";
 import { getRecipeList } from "../function/GetRecipeList";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { HeroHeader } from "../header/HeroHeader";
+import { AddRecipeButton } from "../button/AddRecipeButton";
+import { RecipeCard } from "../card/RecipeCard";
+import { ScrollToTopButton } from "../button/ScrollToTopButton";
 
 export const RecipeListPage: React.FC<RecipeListPageProps> = ({ category, backgroundImage, title, description }) => {
 
@@ -38,6 +42,31 @@ export const RecipeListPage: React.FC<RecipeListPageProps> = ({ category, backgr
     }
     
     return (
-        <></>
+        <>
+            <HeroHeader backgroundImage={backgroundImage} title={title} description={description} descriptionFontSize={26} />
+            <Box>
+                <Typography paddingBottom={3} fontWeight="bold" variant="h4">Recipe List</Typography>
+                <AddRecipeButton currentCategory={category} />
+                {recipeList.length > 0 ? (
+                    <Grid columns={{ xs: 6, sm: 8, md: 12 }} container columnSpacing={6} pt={4} alignItems="center" justifyContent="center">
+                        {recipeList.map((recipe, index) => (
+                            <Grid item xs={6} sm="auto" md="auto" key={index} pb={6}>
+                                <RecipeCard
+                                    title={recipe.title}
+                                    id={recipe.id}
+                                    image={recipe.image}
+                                    createdAt={recipe.createdAt}
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
+                ) : (
+                    <Typography variant="h6" textAlign="center" mt={5}>
+                        現在、表示できるレシピがありません。
+                    </Typography>
+                )}
+            </Box>
+            <ScrollToTopButton />
+        </>
     )
 }
